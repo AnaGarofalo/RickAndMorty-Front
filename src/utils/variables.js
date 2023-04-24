44f8e956cf8bbb1a6876ccdb247ba92b4ad3c473
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-04-24 11:49:43 -0300

    deployment

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,5 +1,5 @@
 // const URL_BASE = "http://localhost:3001/rickandmorty";
 
-const URL_BASE = "https://rickandmorty-back.onrender.com";
+const URL_BASE = "https://rickandmorty-back.onrender.com/rickandmorty";
 
 export default URL_BASE;
