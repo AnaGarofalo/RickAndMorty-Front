8e6964bd368a3f0161db86f380f5faeb5ce75528
Ana María Garófalo <119868532+AnaGarofalo@users.noreply.github.com>
2023-07-25 13:41:33 -0300

    deploy railway

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,7 +1,7 @@
 // const URL_BASE = "http://localhost:3001/rickandmorty";
 
-const URL_BASE = "https://rickandmorty-back.onrender.com/rickandmorty";
+// const URL_BASE = "https://rickandmorty-back.onrender.com/rickandmorty";
 
-// const URL_BASE =
-//   "https://rickandmorty-back-production.up.railway.app/rickandmorty";
+const URL_BASE =
+   "https://rickandmorty-back-production-9228.up.railway.app/rickandmorty";
 export default URL_BASE;
